f06e91df0ad85e6f2167d2cd9755b7e295772ea9
paunov0000 <legendarygriefer96@gmail.com>
2024-04-22 14:25:53 +0300

    imported a custom font

--- TextSplitterApp/tailwind.config.js
+++ TextSplitterApp/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Views/**/*.{cshtml,html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./Views/**/*.{cshtml,html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'site': ["\"Jersey 15\"", "sans-serif",]
+            }
+        },
+    },
+    plugins: [],
 }
 
