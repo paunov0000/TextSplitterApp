d0ba033a49010f83b92b7988bebfd4c87631ad5f
paunov0000 <legendarygriefer96@gmail.com>
2024-04-25 23:42:46 +0300

    Removed logger and removed a using

--- TextSplitterApp/Controllers/HomeController.cs
+++ TextSplitterApp/Controllers/HomeController.cs
@@ -1,18 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using TextSplitterApp.Models;
 
 namespace TextSplitterApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IActionResult Index()
         {
