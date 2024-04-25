46090ccd305d6f8ae80c8345cd8cd5b9ab4ad432
paunov0000 <legendarygriefer96@gmail.com>
2024-04-25 23:43:17 +0300

    Removed Privacy and Error actions

--- TextSplitterApp/Controllers/HomeController.cs
+++ TextSplitterApp/Controllers/HomeController.cs
@@ -25,16 +25,5 @@ namespace TextSplitterApp.Controllers
            
             return View(textViewModel);
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
