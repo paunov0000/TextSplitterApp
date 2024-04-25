a01c58521739ff693c98d1082d2f6ef3ff15a5c3
paunov0000 <legendarygriefer96@gmail.com>
2024-04-25 23:46:53 +0300

    Extended input text from 30 to 50 characters

--- TextSplitterApp/Models/TextViewModel.cs
+++ TextSplitterApp/Models/TextViewModel.cs
@@ -5,7 +5,7 @@ namespace TextSplitterApp.Models
     public class TextViewModel
     {
         [Required(ErrorMessage = "The {0} field is required.")]
-        [StringLength(30, MinimumLength = 2, ErrorMessage = "The field Text must be a string with a minimum length of {2} and maximum length of {1}.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "The field Text must be a string with a minimum length of {2} and maximum length of {1}.")]
         public string Text { get; set; } = null!;
 
         public string SplitText { get; set; } = string.Empty;
